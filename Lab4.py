33d533b4c694a17f3c8d1b22caf47232dfcb2260
DarkDev-H <devinheinemann@gmail.com>
2026-02-11 13:34:54 -0800

    My progress 2/11 1:30

--- Lab4.py
+++ Lab4.py
@@ -6,23 +6,40 @@
 import check_input
 
 def read_maze():
-    file = open("maze.txt", "r")
-    maze = []
+    """Opens the maze.txt file and saves it as a 2d list.
+    Parameters: None
+    Returns: A 2d list representing the maze.
+    """
+    file = open("maze.txt", "r") #Not sure why this isn't opening the file.
+    maze = [] 
+    #Fills the maze as a 2d list from the maze.txt file.
     for line in file:
-        row = []
-        for char in line.strip():
-            row.append(char)
+        row = [] 
+        for char in line.strip(): 
+            if char != '\n': 
+                row.append(char)
         maze.append(row)
     file.close
+    return maze
 
-    print(maze)
+
+def find_start(maze):
+    """Finds where an 's' is in the maze and returns its location as 2d coordinate list.
+    Parameters: A 2d list representing the maze.
+    Returns: A 2d list representing the coordinates of the 's' in the maze.
+    """
+    for i in range (len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == 's':
+                coordinates = [i, j]
+                return coordinates
 
 
-#def find_start(maze):
-#def display_maze(maze, loc):
+def display_maze(maze, loc):
+    return
+
 
 def main():
-    #Test
-    read_maze()
+    location = find_start(read_maze)
 
 main()
