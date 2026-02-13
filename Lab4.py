c04daa1653281333bbf74eb912682dcb8b36c033
Nick T. <nicholastee817@yahoo.com>
2026-02-12 19:53:41 -0800

    Completed game; implemented the while loop to allow user to navigate to 'f'

--- Lab4.py
+++ Lab4.py
@@ -1,9 +1,10 @@
 #Nicholas Terrazas and Devin Heinemann
 #Lab 4
 #02/09/2026
-#Description: ___
+#Description: This program reads a maze from a file into a 2D list and allows the user 
+#to navigate from the start ('s') to the finish ('f') while preventing movement through walls.
 
-import check_input
+from check_input import get_int_range
 
 def read_maze():
     """Opens the maze.txt file and saves it as a 2d list.
@@ -19,7 +20,7 @@ def read_maze():
             if char != '\n': 
                 row.append(char)
         maze.append(row)
-    file.close
+    file.close()
     return maze
 
 
@@ -36,6 +37,12 @@ def find_start(maze):
 
 
 def display_maze(maze, loc):
+    """Displays the maze, showing 'X' at the user's current location.
+    Parameters:
+      maze: 2d list of chars
+      loc: 1d list [row, col]
+    Returns: None
+    """
     locX = loc[0]
     locY = loc[1]
 
@@ -52,7 +59,57 @@ def display_maze(maze, loc):
 def main():
     maze = read_maze()
     location = find_start(maze)
-    display_maze(maze, location)
 
+    play = True
+    choice = 0
 
+    print("-Maze Solver-")
+
+    while(play):
+        currentX = location[0]
+        currentY = location[1]
+
+        display_maze(maze, location)
+
+        print("1. Go North\n"+"2. Go South\n"+"3. Go East\n"+"4. Go West\n"+"5. Quit")
+        choice = get_int_range("Enter choice: ", 1, 5)
+
+        #Go North
+        if(choice == 1):
+            if(maze[currentX - 1][currentY] == '*'):
+                print("You cannot move there.")
+                continue
+            location = [currentX - 1, currentY]
+
+        #Go South
+        if(choice == 2):
+            if(maze[currentX + 1][currentY] == '*'):
+                print("You cannot move there.")
+                continue
+            location = [currentX + 1, currentY]
+
+        #Go East
+        if(choice == 3):
+            if(maze[currentX][currentY + 1] == '*'):
+                print("You cannot move there.")
+                continue
+            location = [currentX, currentY + 1]
+
+        #Go West
+        if(choice == 4):
+            if(maze[currentX][currentY - 1] == '*'):
+                print("You cannot move there.")
+                continue
+            location = [currentX, currentY - 1]
+
+        #Quit game
+        if(choice == 5): 
+            play = False
+
+        #Checks for finish
+        if maze[location[0]][location[1]] == 'f':
+            display_maze(maze, location)
+            print("Congratulations! You solved the maze!")
+            play = False
+    print("Thanks for playing!\n") 
 main()
