a502d88c0f1ac319ce876d9b49ddf4a6754b6247
Nick T. <nicholastee817@yahoo.com>
2026-03-02 13:01:39 -0800

    Added main source code; still need to merge take_turn function to test

--- Lab7/main.py
+++ Lab7/main.py
@@ -2,15 +2,24 @@
 #Lab 7
 #03/02/2026
 
+
 import player
 import check_input
 
-def take_turn 
-
-#testing remote branch
+#def take_turn(player):
 
 def main():
-
+    player1 = player.Player()
+    play = True
+    #Loop to take turns until the user chooses to end the game
+    while play:
+        take_turn(player1)
+        cont = check_input.get_yes_no("Do you want to continue? (y/n): ")
+        if cont == False:
+            play = False
+        
+    print("Game Over.")
+    print("Final Score = " + str(player1.score))
 
 if __name__ == "__main__":
     main()
