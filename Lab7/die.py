ef1df096c5704f7ed2bae4ece3e58db1c74daed9
Nick T. <nicholastee817@yahoo.com>
2026-03-02 12:12:12 -0800

    Added some text indentifiers to each file

---
+++ Lab7/die.py
@@ -0,0 +1,2 @@
+#Nicholas Terrazas and Devin Heinemann
+#03/02/2026
---
+++ Lab7/main.py
@@ -0,0 +1,10 @@
+#Nicholas Terrazas and Devin Heinemann
+#Lab 7
+#03/02/2026
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
