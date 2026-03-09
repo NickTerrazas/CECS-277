f7daab9524b312b3b0ea59ad672a5122a66e3246
DarkDev-H <devinheinemann@gmail.com>
2026-03-09 13:19:01 -0700

    Main Game Loop working. Need to get the attacks and damage working.

--- Lab8/main.py
+++ Lab8/main.py
@@ -7,6 +7,7 @@ from flying import FlyingDragon
 from dragon import Dragon
 from entity import Entity
 from hero import Hero
+import check_input
 
 
 def main():
@@ -18,10 +19,18 @@ def main():
 
     print(f"Welcome to dragon training, {hero_name}!\nYou must defeat 3 dragons!\n")
 
-    print(hero.__str__())
-    print("1. Attack " + dragons[0].__str__())
-    print("2. Attack " + dragons[1].__str__())
-    print("3. Attack " + dragons[2].__str__())
+    while hero._hp > 0 and len(dragons) > 0:
+        print(hero.__str__())
+        if len(dragons) > 0:
+            print(" 1. Attack " + dragons[0].__str__())
+        if len(dragons) > 1:
+            print(" 2. Attack " + dragons[1].__str__())
+        if len(dragons) > 2:
+            print(" 3. Attack " + dragons[2].__str__())
+        d_choice = check_input.get_int_range("Which dragon do you want to attack: ", 1, len(dragons))
+
+        print("Attack with:\n 1. Arrow (1 D12)\n 2. Sword (2 D6)")
+        w_choice = check_input.get_int_range("Enter weapon: ", 1, 2)
 
 
 if __name__ == "__main__":
