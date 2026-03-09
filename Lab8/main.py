28a77c2245d7026c0399f0419103219b7d9ea721
Nick T. <nicholastee817@yahoo.com>
2026-03-09 12:50:23 -0700

    Started work on main, still need to properly integrate the different classes

---
+++ Lab8/main.py
@@ -0,0 +1,27 @@
+#Nicholas Terrazas and Devin Heinemann
+#Lab 8
+#03/09/2026
+
+from fire import FireDragon
+from flying import FlyingDragon
+from dragon import Dragon
+from entity import Entity
+from hero import Hero
+
+
+def main():
+
+    #Create a hero and a list of 3 dragons (1 regular, 1 fire, 1 flying)
+    hero_name = input("What is your name, challenger? ")
+    hero = Hero(hero_name, 50)
+    dragons = [Dragon("Deadly Nadder", 10), FireDragon("Gronkle", 15), FlyingDragon("Timberjack", 20)]
+
+    print(f"Welcome to dragon training, {hero_name}!\n You must defeat 3 dragons!")
+
+    print(f"{hero_name}: {hero.hp}/{hero.max_hp}")
+    
+
+
+
+    if __name__ == "__main__":
+        main()
