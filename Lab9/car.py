c4ac0a425ce8a1d81bcb48e4100a4c6d5c7cc264
Nick T. <nicholastee817@yahoo.com>
2026-03-16 13:02:44 -0700

    Added some stuff to main; added underscores in car class

--- Lab9/car.py
+++ Lab9/car.py
@@ -12,15 +12,15 @@ class Car(vehicle.Vehicle):
         
         """
         #Determine if the car has enough energy.
-        if super().energy >= 15:
-            super().energy -= 15
+        if super()._energy >= 15:
+            super()._energy -= 15
             distance = round(super().speed * 1.5) + random.randint(-1, 1)
             #Determine if the car hits an obstacle
-            if (super().position + distance) >= obs_loc:
-                super().position = obs_loc
-                return str(super().name) + " crashes into an obstacle."
+            if (super()._position + distance) >= obs_loc:
+                super()._position = obs_loc
+                return str(super()._name) + " crashes into an obstacle."
             else:
-                return str(super().name) + " uses nitro boost and moves " + str(distance) + " units."
+                return str(super()._name) + " uses nitro boost and moves " + str(distance) + " units."
         else:
-            super().position += 1
-            return str(super().name) + " did not have enough energy for a nitro boost and only moves 1 unit."
+            super()._position += 1
+            return str(super()._name) + " did not have enough energy for a nitro boost and only moves 1 unit."
--- Lab9/main.py
+++ Lab9/main.py
@@ -3,22 +3,60 @@
 #03/16/2026
 
 # Description: 
+'''
+
+'''
 
 import abc
 import random
 from check_input import get_int_range
-import vehicle
 import car
 import motorcycle
 import truck
 
 def main():
+    c = car.Car("Lightning Car", "C", 7)
+    b = motorcycle.Motorcycle("Swift Bike", "M", 8)
+    t = truck.Truck("Behemoth Truck", "T", 6)
+
+    track = [["_" for i in range(100)] for j in range(3)]
+    for i in range(3):
+        for j in range(2):
+            obs_loc = random.randint(1, 98)
+            track[i][obs_loc] = "O"
+    
+
     print("Rad Racer!")
     print("Choose a vehicle and race it down the track (player = 'P'). Slow down for obstacles ('O') or else you'll crash!")
     print("1. Lightning Car - a fast car. Speed: 7. Special: Nitro Boost (1.5x speed).")
     print("2. Swift Bike - a speedy motorcycle. Speed: 8. Special: Wheelie (2x speed but there's a chance you'll wipe out).")
     print("3. Behemoth Truck - a heavy truck. Speed: 6. Special: Ram (2x speed and it smashes through obstacles).")
     choice = get_int_range("Choose your vehicle (1-3): ", 1, 3)
+
+    if choice == 1:
+        player = c
+        player._position = 0
+    elif choice == 2:
+        player = b
+        player._position = 0
+    else:
+        player = t
+        player._position = 0
+
+
+
+    for i in range(3):
+        for j in range(100):
+            if track[i][j] == player._initial:
+                track[i][j] = "P"
+            elif track[i][j] == "O":
+                continue
+            else:
+                track[i][j] = "_"
+
+    for lane in track:
+        print(" ".join(lane))
+
 
 
 
