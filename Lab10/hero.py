1c3cb2c3e28eef675ce07eb8425257c559f3005c
DarkDev-H <devinheinemann@gmail.com>
2026-04-06 12:34:35 -0700

    Hero Class created. Needs docstrings. Needs testing.

---
+++ Lab10/hero.py
@@ -0,0 +1,58 @@
+import maze
+
+class Hero():
+    """A class to represent a hero in a game."""
+
+    def __init__(self):
+        """init"""
+        maze = maze.Maze()
+        rownum = 0
+        for row in maze:
+            colnum = 0
+            for col in row:
+                if col == 's':
+                    maze[row][col] = 'H'
+                    self._row = rownum
+                    self._col = colnum
+                colnum += 1
+            rownum += 1
+
+    def go_up(self):
+        """Move the hero up."""
+        if self._row > 0:
+            new_loc = maze[self._row - 1][self._col]
+            if new_loc != '*':
+                maze[self._row][self._col] = ' '
+                self._row -= 1
+                maze[self._row][self._col] = 'H'
+            return str(new_loc)
+
+    def go_down(self):
+        """Move the hero down."""
+        if self._row < len(maze) - 1:
+            new_loc = maze[self._row + 1][self._col]
+            if new_loc != '*':
+                maze[self._row][self._col] = ' '
+                self._row += 1
+                maze[self._row][self._col] = 'H'
+            return str(new_loc)
+
+    def go_left(self):
+        """Move the hero left."""
+        if self._col > 0:
+            new_loc = maze[self._row][self._col - 1]
+            if new_loc != '*':
+                maze[self._row][self._col] = ' '
+                self._col -= 1
+                maze[self._row][self._col] = 'H'
+            return str(new_loc)
+
+    def go_right(self):
+        """Move the hero right."""
+        if self._col < len(maze[0]) - 1:
+            new_loc = maze[self._row][self._col + 1]
+            if new_loc != '*':
+                maze[self._row][self._col] = ' '
+                self._col += 1
+                maze[self._row][self._col] = 'H'
+            return str(new_loc)
