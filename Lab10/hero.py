d981693b9ae6489307deb38bc5b3b43c0872c395
DarkDev-H <devinheinemann@gmail.com>
2026-04-08 12:35:51 -0700

    Fixed Hero Movement

--- Lab10/hero.py
+++ Lab10/hero.py
@@ -7,49 +7,51 @@ class Hero():
         """Locates the starting location and places the hero there."""
         #super().__init__()
         mazeob = maze.Maze()
-
         start = mazeob.search_maze('s')
         self._row = start[0]
         self._col = start[1]
         mazeob[self._row][self._col] = 'H'
 
-
     def go_up(self):
         """Move the hero up."""
+        mazeob = maze.Maze()
         if self._row > 0:
-            new_loc = maze[self._row - 1][self._col]
+            new_loc = mazeob[self._row - 1][self._col]
             if new_loc != '*':
-                maze[self._row][self._col] = ' '
+                mazeob[self._row][self._col] = ' '
                 self._row -= 1
-                maze[self._row][self._col] = 'H'
+                mazeob[self._row][self._col] = 'H'
             return str(new_loc)
 
     def go_down(self):
         """Move the hero down."""
-        if self._row < len(maze) - 1:
-            new_loc = maze[self._row + 1][self._col]
+        mazeob = maze.Maze()
+        if self._row < len(mazeob) - 1:
+            new_loc = mazeob[self._row + 1][self._col]
             if new_loc != '*':
-                maze[self._row][self._col] = ' '
+                mazeob[self._row][self._col] = ' '
                 self._row += 1
-                maze[self._row][self._col] = 'H'
+                mazeob[self._row][self._col] = 'H'
             return str(new_loc)
 
     def go_left(self):
         """Move the hero left."""
+        mazeob = maze.Maze()
         if self._col > 0:
-            new_loc = maze[self._row][self._col - 1]
+            new_loc = mazeob[self._row][self._col - 1]
             if new_loc != '*':
-                maze[self._row][self._col] = ' '
+                mazeob[self._row][self._col] = ' '
                 self._col -= 1
-                maze[self._row][self._col] = 'H'
+                mazeob[self._row][self._col] = 'H'
             return str(new_loc)
 
     def go_right(self):
         """Move the hero right."""
-        if self._col < len(maze[0]) - 1:
-            new_loc = maze[self._row][self._col + 1]
+        mazeob = maze.Maze()
+        if self._col < len(mazeob[0]) - 1:
+            new_loc = mazeob[self._row][self._col + 1]
             if new_loc != '*':
-                maze[self._row][self._col] = ' '
+                mazeob[self._row][self._col] = ' '
                 self._col += 1
-                maze[self._row][self._col] = 'H'
+                mazeob[self._row][self._col] = 'H'
             return str(new_loc)
