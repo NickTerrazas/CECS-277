5bd86187ef6563148f61e0c680e226373805bbfd
Nick T. <nicholastee817@yahoo.com>
2026-04-13 13:05:37 -0700

    Fixed expert factory; creates objects after import

--- Lab11/exp_factory.py
+++ Lab11/exp_factory.py
@@ -1,13 +1,16 @@
 import enemy_factory
 import random
+import goblin
+import ogre
+import troll
 
 class ExpertFactory(enemy_factory.EnemyFactory):
     #randomly constructs and returns one of the expert enemies. Goblin, Ogre or Troll.
     def create_random_enemy(self):
         enemy_type = random.choice(['goblin', 'ogre', 'troll'])
         if enemy_type == 'goblin':
-            return Goblin()
+            return goblin.Goblin()
         elif enemy_type == 'ogre':
-            return Ogre()
+            return ogre.Ogre()
         elif enemy_type == 'troll':
-            return Troll()
+            return troll.Troll()
