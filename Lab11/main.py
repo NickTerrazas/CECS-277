f98a6c8c4d628f80634a7fd61860405a30e1bedf
Nick T. <nicholastee817@yahoo.com>
2026-04-15 12:55:24 -0700

    Formatted the output for the output

--- Lab11/main.py
+++ Lab11/main.py
@@ -14,15 +14,16 @@ import hero
 
 
 def main():
-    print("Monster Trials")
+    print("Monster Trials\n")
 
     #Ask user for their name and create a hero with that name.
     name = input("What is your name? ")
     Player = hero.Hero(name)
+    print()
     print(f"Welcome, {name}, to the Monster Trials! You will be fighting against 3 monsters. Defeat them all to win!")
 
     #Ask user for the difficulty they want to play on. If they choose beginner, create a BegFactory. If they choose expert, create an ExpertFactory.
-    difficulty = check_input.get_int_range("Choose your difficulty: \n1. Beginner\n2. Expert\n", 1, 2)
+    difficulty = check_input.get_int_range("\nChoose your difficulty: \n1. Beginner\n2. Expert\n", 1, 2)
     if difficulty == 1:
         factory = beg_factory.BeginnerFactory()
     else:
